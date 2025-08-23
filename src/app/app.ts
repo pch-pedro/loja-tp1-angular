b81f9dcac45dece8f4de26f5f6beb0364b13ff16
pch-pedro <sp.pedro.pacheco23.3@gmail.com>
2025-08-22 21:38:34 -0300

    FinalAula1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/header/header";
+import { Footer } from "./core/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
