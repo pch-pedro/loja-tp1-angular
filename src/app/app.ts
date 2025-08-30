49eb977432692fe8bc9ace087c94c64bb0f7b4a0
pch-pedro <sp.pedro.pacheco23.3@gmail.com>
2025-08-29 22:16:57 -0300

    Final Aula 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,14 +5,19 @@ import { Footer } from "./core/footer/footer";
 import { Banner } from './core/banner/banner';
 import { TestComponentRenderer } from '@angular/core/testing';
 import { QuantidadeControle } from "./shared/quantidade-controle/quantidade-controle";
+import { Produto } from './model/produto';
+import { CardProduto } from "./features/produtos/card-produto/card-produto";
+import { ListaProdutos } from "./features/produtos/lista-produtos/lista-produtos";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Banner, QuantidadeControle],
+  imports: [Header, Footer, Banner, ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  
   protected readonly title = signal('loja-tp1-angular');
   sobre? :string;
 
