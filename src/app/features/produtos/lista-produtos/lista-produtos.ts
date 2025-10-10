3bb6844a3a1b64ef300b8d2f39fa8f6a0decfedc
pch-pedro <sp.pedro.pacheco23.3@gmail.com>
2025-10-10 20:22:11 -0300

    Correção

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -4,6 +4,7 @@ import { CardProduto } from "../card-produto/card-produto";
 import { ProdutoService } from '../services/produto.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'lista-produtos',
@@ -17,7 +18,7 @@ export class ListaProdutos {
   private router = inject(Router);
   loading = signal(true);
 
-  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar(), {initialValue: []});
+  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar().pipe(finalize(() => this.loading.set(false))), {initialValue: []});
 
   apenasPromo = signal(false);
 
